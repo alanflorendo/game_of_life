32a8073556f26fcef3408b949d9af82d90fb8c0d
alanflorendo <alan.florendo@gmail.com>
2014-11-10 09:21:42 -0600

    Cells can die and even regenerate

--- cell.rb
+++ cell.rb
@@ -1,12 +1,20 @@
 class Cell
 	attr_reader :living
 
-	def initialize
-		@living = true if rand > 0.5
+	def initialize(living)
+		@living = living
 	end
 
 	def dead
 		return !living
+	end
+
+	def dies
+		@living = false
+	end
+
+	def regenerates
+		@living = true
 	end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,20 +2,34 @@ require 'spec_helper'
 require_relative '../cell.rb'
 
 describe 'Cell' do
-	let!(:new_cell) { Cell.new }
+	let!(:new_living_cell) { Cell.new(true) }
+	let!(:new_dead_cell) { Cell.new(false) }
 
 	context "when initialized" do 
 
 		it "is a Cell object" do
-			expect(new_cell).to be_a(Cell)
+			expect(new_living_cell).to be_a(Cell)
 		end
 	end
 
 	context "in life and death matters" do
-		it "is either dead or alive" do
-			expect(new_cell).to respond_to(:living)
-			expect(new_cell).to respond_to(:dead)
+		it "is either dead or living" do
+			expect(new_living_cell).to respond_to(:living)
+			expect(new_living_cell).to respond_to(:dead)
 		end
+
+		it "is dead (not living) after it dies" do
+			new_living_cell.dies
+			expect(new_living_cell.dead).to eq(true)
+			expect(new_living_cell.living).to eq(false)
+		end
+
+		it "is living (not dead) after it regenerates" do
+			new_dead_cell.regenerates
+			expect(new_dead_cell.living).to eq(true)
+			expect(new_dead_cell.dead).to eq(false)
+		end
+
 	end
 
 
