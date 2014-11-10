5cbb778bdde9c7453c7f45a2ca267f40fb6121fd
alanflorendo <alan.florendo@gmail.com>
2014-11-10 09:54:29 -0600

    The entire environment can easily be populated by cells (living or dead)
    Cells populate environment in a two-dimensional array

--- environment.rb
+++ environment.rb
@@ -1,9 +1,32 @@
+require_relative 'cell.rb'
+
 class Environment
-	attr_reader :height, :width
+	attr_reader :height, :width, :cells
 
 	def initialize(height, width)
 		@height = height
 		@width = width
+		@cells = []
+		@height.times { @cells.push(Array.new(width))}
+	end
+
+	def culture_cells
+		@height.times do |row|
+			@cells.push([])
+			@width.times do |cell|
+				@cells[row].push(Cell.new(true))
+			end			
+		end
+	end
+
+	def to_s
+		@height.times do |row|
+			@width.times do |col|
+				print @cells[row][col]
+			end
+			print "\n"
+		end
 	end
 
 end
+
--- spec/environment_spec.rb
+++ spec/environment_spec.rb
@@ -4,7 +4,7 @@ require_relative '../environment.rb'
 describe 'Environment' do
 	let!(:new_environment) { Environment.new(5,10) }
 
-	context "when initialized" do 
+	context "when initialized as 5x10" do 
 
 		it "is a Cell object" do
 			expect(new_environment).to be_an(Environment)
@@ -14,6 +14,17 @@ describe 'Environment' do
 			expect(new_environment.height).to eq(5)
 			expect(new_environment.width).to eq(10)
 		end
+
+		it "has a size of 25 (5x5)" do
+			expect(new_environment.cells.flatten.length).to eq(50)
+		end
+
+		it "has an empty population (ie, no cells)" do
+			new_environment.cells.flatten.each { |cell| 
+				expect(cell).to eq(nil) }
+		end
+
+
 	end
 
 end
