791b5aae1a0fd3466597387288f9195d1b77b5e8
alanflorendo <alan.florendo@gmail.com>
2014-11-10 22:07:40 -0600

    Add new shapes (toad, beacon, spaceship/glider)

--- driver.rb
+++ driver.rb
@@ -3,12 +3,12 @@ require_relative 'environment.rb'
 puts "\e[H\e[2J" #clear_screen before driver code
 
 # BLINKER (PERIOD 2)
-e = Environment.new(5, 5, 0)
-e.culture_cells
-e.culture_cell(1,2)
-e.culture_cell(2,2)
-e.culture_cell(3,2)
-e.tick_indefinitely(0.5)
+# e = Environment.new(5, 5, 0)
+# e.culture_cells
+# e.culture_cell(1,2)
+# e.culture_cell(2,2)
+# e.culture_cell(3,2)
+# e.tick_indefinitely(0.5)
 
 
 # STILL LIFE (BLOCK)
@@ -35,5 +35,38 @@ e.tick_indefinitely(0.5)
 # e.culture_cell(2,4)
 # e.culture_cell(3,2)
 # e.culture_cell(3,3)
-# e.tick_indefinitely(5)
+# e.tick_indefinitely(0.5)
 
+# TOAD (PERIOD 2)
+# e = Environment.new(6,6,0)
+# e.culture_cells
+# e.culture_cell(2,2)
+# e.culture_cell(2,3)
+# e.culture_cell(2,4)
+# e.culture_cell(3,3)
+# e.culture_cell(3,4)
+# e.culture_cell(3,5)
+# e.tick_indefinitely(0.5)
+
+# BEACON (PERIOD 2)
+# e = Environment.new(6,6,0)
+# e.culture_cells
+# e.culture_cell(1,1)
+# e.culture_cell(1,2)
+# e.culture_cell(2,1)
+# e.culture_cell(2,2)
+# e.culture_cell(3,3)
+# e.culture_cell(3,4)
+# e.culture_cell(4,3)
+# e.culture_cell(4,4)
+# e.tick_indefinitely(0.5)
+
+# GLIDER
+e = Environment.new(40,40,0)
+e.culture_cells
+e.culture_cell(0,1)
+e.culture_cell(1,2)
+e.culture_cell(2,0)
+e.culture_cell(2,1)
+e.culture_cell(2,2)
+e.tick_indefinitely(0.25)
